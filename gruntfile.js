42490c486a364ca3d4c8c53cbee6bf7ccb222c1e
nbpetts <nbpetts@gmail.com>
2015-05-21 11:36:39 -0500

    set up bootstrap and grunt

---
+++ gruntfile.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    project: {
+//      app: [''],
+//      assets: ['<%= project.app %>'],
+      css: ['sass/my_styles.scss']
+    },
+    sass: {
+      dev: {
+        options: {
+          style: 'expanded',
+          compass: false
+        },
+        files: {
+          'css/my_styles.css': 'sass/my_styles.scss'
+        }
+      }
+    },
+    watch: {
+      sass: {
+        files: 'sass/{,*/}*.{scss,sass}',
+        tasks: ['sass:dev']
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', [
+      'watch'
+    ]);
+
+};
