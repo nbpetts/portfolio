5eb0aa530d70b3b7f97c4cc065f3e9d1693b0558
nbpetts <nbpetts@gmail.com>
2015-06-03 11:02:13 -0500

    footer fix

--- includes/footer.php
+++ includes/footer.php
@@ -1,40 +1,40 @@
 <footer class=" ">
 
-      <section class="row bottomAlign">
-        <img src="images/pixelGuyEyesRight.png" alt="Pixel Version of Nathan Petts" class="img-responsive col-xs-2 col-xs-offset-1 nathanFooter  ">
-        <section class="row col-xs-8">
-        <ul class="footerLinks nav nav-pills col-xs-12 ">
-          <li class=""><a href="portfolio.php">My Work</a>
-          </li>
-          <li class=""><a href="resume.php">My Résumé</a>
-          </li>
-          <li class=""><a href="about.php">About Me</a>
-          </li>
-          <li class=""><a href="contact.php">Contact Me</a>
-          </li>
-          <p class="col-xs-12 copyright">Site <span class="glyphicon glyphicon-copyright-mark"></span> <?php date_default_timezone_set('UTC'); echo date("Y"); ?> Nathan Petts.  <br>Maniac Mansion is a copyright of its respective owners. Please do not sue me. </p>
-        </ul>
-         </section>
-        <img src="images/tree.png" class="img-responisve hidden-xs col-sm-8 col-sm-pull-1 footerTree" alt="A pretty tree">
-      </section>
-  
-   
-  
-      <!--end row-->
+  <section class="row bottomAlign">
+    <div class="col-xs-2 col-xs-offset-1"><img src="images/pixelGuyEyesRight.png" alt="Pixel Version of Nathan Petts" class="img-responsive  nathanFooter  ">
+    </div>
+    <div class="row col-xs-8">
+      <ul class="footerLinks nav nav-pills col-xs-12 ">
+        <li class=""><a href="portfolio.php">My Work</a>
+        </li>
+        <li class=""><a href="resume.php">My Résumé</a>
+        </li>
+        <li class=""><a href="about.php">About Me</a>
+        </li>
+        <li class=""><a href="contact.php">Contact Me</a>
+        </li>
+        <p class="col-xs-12 copyright">Site <span class="glyphicon glyphicon-copyright-mark"></span>
+          <?php date_default_timezone_set( 'UTC'); echo date( "Y"); ?> Nathan Petts.
+          <br>Maniac Mansion is a copyright of its respective owners. Please do not sue me. </p>
+      </ul>
+    </div>
+    <div class="hidden-xs col-sm-8 col-sm-pull-1"><img src="images/tree.png" class="img-responisve  footerTree" alt="A pretty tree">
+    </div>
+  </section>
 
 
 
-      <section class="row">
-        <div class="col-xs-12 grass "></div>
-
-
-      </section>
+  <!--end row-->
 
 
 
-    </footer>
-    <!--end footer-->
- 
-    
+  <section class="row">
+    <div class="col-xs-12 grass "></div>
 
 
+  </section>
+
+
+
+</footer>
+<!--end footer-->
