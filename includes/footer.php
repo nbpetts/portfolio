a26ddf5ba9b2525bc4640cf0cfc4a674bb95ef38
nbpetts <nbpetts@gmail.com>
2015-06-03 12:04:08 -0500

    footer fixin

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,11 @@
 <footer class=" ">
 
   <section class="row bottomAlign">
-    <div class="col-xs-2 col-xs-offset-1"><img src="images/pixelGuyEyesRight.png" alt="Pixel Version of Nathan Petts" class="img-responsive  nathanFooter  ">
+    <div class="col-xs-1  col-sm-2">
+      <img src="images/pixelGuyEyesRight.png" alt="Pixel Version of Nathan Petts" class="img-responsive  nathanFooter  ">
     </div>
-    <div class="row col-xs-8">
-      <ul class="footerLinks nav nav-pills col-xs-12 ">
+    <div class="row col-xs-8 col-xs-offset-1 col-sm-6 col-sm-offset-0 col-md-7">
+      <ul class="footerLinks nav nav-pills  ">
         <li class=""><a href="portfolio.php">My Work</a>
         </li>
         <li class=""><a href="resume.php">My Résumé</a>
@@ -18,7 +19,7 @@
           <br>Maniac Mansion is a copyright of its respective owners. Please do not sue me. </p>
       </ul>
     </div>
-    <div class="hidden-xs col-sm-8 col-sm-pull-1"><img src="images/tree.png" class="img-responisve  footerTree" alt="A pretty tree">
+    <div class="hidden-xs col-sm-8 col-md-push-1 col-lg-push-1"><img src="images/tree.png" class="img-responisve  footerTree" alt="A pretty tree">
     </div>
   </section>
 
