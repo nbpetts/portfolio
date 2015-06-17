9fa39fe38c2f59638fc2bb22c322f8a0d60be9ee
nbpetts <nbpetts@gmail.com>
2015-06-17 13:44:04 -0500

    added new button styles

--- includes/portfolioButton.php
+++ includes/portfolioButton.php
@@ -1,4 +1,4 @@
 
-        <div class="col-xs-12 col-sm-12 col-lg-4 col-sm-offset-1">
-          <a class="btn btn-default" href="portfolio.php" role="button">Peruse my<br>Full Portfolio</a>
+        <div class="col-xs-6 col-sm-4 col-lg-4 col-sm-offset-1 buttonDiv">
+          <a class="btn btn-default btn-block" href="portfolio.php" role="button">Peruse my Full Portfolio</a>
         </div>
