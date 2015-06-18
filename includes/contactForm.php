e2d57a5c6bd869391936355080da1d3dcf846d2d
Nathan Petts <nbpetts@gmail.com>
2015-06-17 19:54:49 -0500

    finished button styling

--- includes/contactForm.php
+++ includes/contactForm.php
@@ -1,37 +1,42 @@
 <section class="row">
-      <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-sm-8 ">
+    <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-sm-8 ">
         <h1>Drop me a line, why not?</h1>
         <a name="content"></a>
-      </div>
+    </div>
 
-    </section>
+</section>
 <section class="row">
-<form class="col-xs-12 col-sm-10 col-sm-offset-1 col-sm-8 " name="contactform" method="post" action="sendEmailForm.php">
+    <form class="col-xs-12 col-sm-10 col-sm-offset-1 col-sm-8 " name="contactform" method="post" action="sendEmailForm.php">
 
-    <ul class="myForm">
-        <li class="myLabel">
-            <input type="text" class="" id="Name" name="Name" placeholder="" />
-            <label for="Name">Your Name</label>
-        </li>
-        <li class="myLabel">
-            <input type="email" class="" id="Email1" name="Email" placeholder="" />
-            <label for="Email1">Your Email - Required</label>
-        </li>
-        <li class="myLabel">
+        <ul class="myForm">
+            <li class="myLabel">
+                <input type="text" class="" id="Name" name="Name" placeholder="" />
+                <label for="Name">Your Name</label>
+            </li>
+            <li class="myLabel">
+                <input type="email" class="" id="Email1" name="Email" placeholder="" />
+                <label for="Email1">Your Email - Required</label>
+            </li>
+            <li class="myLabel">
 
-            <textarea type="text" class="col-xs-12" id="Message" name="Message" placeholder=""></textarea>
-            <label for="Message">Your Message</label>
-        </li>
-        <li class="myLabel">
-            <div class="checkbox hidden" aria-hidden="true">
-                <label aria-hidden="true"> Screen readers, please ignore this. 
-                    <input type="checkbox" name="Check">Check me out
-                </label>
+                <textarea type="text" class="col-xs-12" id="Message" name="Message" placeholder=""></textarea>
+                <label for="Message">Your Message</label>
+            </li>
+            <li class="myLabel">
+                <div class="checkbox hidden" aria-hidden="true">
+                    <label aria-hidden="true">Screen readers, please ignore this.
+                        <input type="checkbox" name="Check">Check me out
+                    </label>
+                </div>
+            </li>
+
+        </ul>
+        <div class="row">
+            <div class="col-xs-6 col-sm-4 col-lg-4">
+                <button type="submit" class=" btn btn-block  ">Submit</button>
             </div>
-        </li>
 
-    </ul>
+        </div>
 
-    <button type="submit" class="btn btn-default">Submit</button>
-</form>
-  </section>
+    </form>
+</section>
--- includes/resumeButton.php
+++ includes/resumeButton.php
@@ -1,4 +1,6 @@
+<!--<button class="col-xs-12 col-sm-4 col-sm-offset-2 col-lg-4  btn btn-default" href="resume.php" role="button">Read my Résumé</button>-->
 
-        <div class="col-xs-12 col-sm-12 col-lg-4 col-sm-offset-1">
-          <a class="btn btn-default" href="resume.php" role="button">Read my Résumé</a>
+
+        <div class="col-xs-6 col-sm-4 col-lg-4 col-sm-offset-1 buttonDiv">
+          <a class="btn btn-default btn-block" href="resume.php" role="button">Read my Résumé</a>
         </div>
