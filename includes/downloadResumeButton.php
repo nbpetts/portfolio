6219c12fcd64b3c06a58af29bd89d5738026206c
Nathan Petts <nbpetts@gmail.com>
2015-06-28 09:19:03 -0500

    download resume button

---
+++ includes/downloadResumeButton.php
@@ -0,0 +1,3 @@
+<div class="col-xs-4 col-sm-4 col-lg-4  buttonDiv">
+          <a class="btn btn-default btn-block backToPortfolioButton" href="download.php?file=Nathan_Petts_Resume.pdf" role="button">Download my Résumé</a>
+        </div>
--- resume.php
+++ resume.php
@@ -2,21 +2,22 @@
 <html lang="en">
 
 <head>
-<?php include "includes/head.php" ?>
+  <?php include "includes/head.php" ?>
   <title>Résumé | Nathan Petts - UX designer</title>
- 
+
 
 </head>
 
 <body data-spy="scroll" data-target=".navbar">
-  <?php include_once("includes/googleAnalytics.php") ?>
+  <?php include_once( "includes/googleAnalytics.php") ?>
   <?php include "includes/header.php" ?>
-  
+
   <div class="container pageBackground">
 
     <section class="row ">
       <div class="col-xs-12 col-sm-8 ">
-        <h1>Nathan B. Petts</h1><a id="content" name="content"></a> 
+        <h1>Nathan B. Petts</h1>
+        <a id="content" name="content"></a>
         <a href="mailto:nbpetts@gmail.com" class="resumeEmail">nbpetts [at] gmail [dot] com</a>
       </div>
 
@@ -24,7 +25,7 @@
     <section class="row">
       <div class="col-xs-12 col-sm-4 ">
         <h2>Experience</h2>
-        
+
       </div>
 
     </section>
@@ -33,12 +34,12 @@
         <h3 class="jobTitle">MATEC Technologist </h3>
         <p class="jobPlace">University of Illinois at Chicago</p>
         <p class="jobTime">March 2014 - Current</p>
-        
+
       </div>
       <div class="col-xs-10 col-xs-offset-1 col-sm-offset-0 col-sm-9 jobDetailsGroup text-left">
-        
+
         <ul>
-          <li> <strong>Web Design &amp; Project Management –</strong> Redesigning the department’s website to make it more usable, accessible, and in line with modern web standards. I work with staff to gather the business requirements and translate those into web design specifications for a contracted vendor. When complete, the users will have a single consolidated place to create content, sign up for training, capture reporting data, and enjoy their time doing it. </li>
+          <li> <strong>Web Design &amp; Project Management –</strong> Redesigning the department’s website to make it more usable, accessible, and in line with modern web standards. I work with staff to gather the business requirements and translate those into web design specifications for a contracted vendor. When complete, the users will have a single consolidated place to create content, sign up for training, capture reporting data, and enjoy their time doing it.
             <ul>
               <li>Completed a content audit as well as accessibility and heuristic evaluations of the site and systems</li>
               <li>Conducted user interviews and contextual analyses to gather user requirements</li>
@@ -46,13 +47,13 @@
               <li>Implemented version control and began content migration and curation </li>
               <li>Implemented a Drupal-based, modern, accessible website</li>
             </ul>
+          </li>
           <li><strong>Technology Champion -</strong> Working with the grant writing team to determine how technology can help educate more clinicians and allow us to do our jobs better.</li>
           <li><strong>Data Management -</strong> Ensuring data integrity by troubleshooting data errors and bugs and training users on effective data collection processes. Provide customized reports using web interfaces, PHP and MySQL. My accurate, timely reports provide management with the information they need for effective decision-making and report writing.</li>
-          <li><strong>Tech Support -</strong> Performing server administration; helping onsite and remote users through technology issues; 
-managing the laptop inventory. Hired graduate student assistant; our technical expertise and troubleshooting skills provide a stable technology environment on which MATEC users can depend.</li>
-        
+          <li><strong>Tech Support -</strong> Performing server administration; helping onsite and remote users through technology issues; managing the laptop inventory. Hired graduate student assistant; our technical expertise and troubleshooting skills provide a stable technology environment on which MATEC users can depend.</li>
+
         </ul>
-        
+
       </div>
 
     </section>
@@ -61,152 +62,146 @@ managing the laptop inventory. Hired graduate student assistant; our technical e
         <h3 class="jobTitle">Technology Support Specialist</h3>
         <p class="jobPlace">Rush University Medical Library</p>
         <p class="jobTime">October 2012 - February 2014</p>
-         </div>
+      </div>
       <div class="col-xs-10 col-xs-offset-1 col-sm-offset-0 col-sm-9 jobDetailsGroup text-left">
-        
+
         <ul>
           <li>Guided the redesign of the library website and participated in executing updates that delivered a highly organized, more intuitive user interface.</li>
           <li>Solely responsible for website content migration from Fatwire CMS to Libguides CMS resulting in positive reviews from user community.</li>
           <li>Provided support for Blackboard Learning Management System, improving patron understanding and increasing their use of the system.</li>
           <li>Helped select, acquire and administer ExamSoft testing analytics software. </li>
-        
+
         </ul>
-        
+
       </div>
 
     </section>
-     <section class="row job">
+    <section class="row job">
       <div class="col-xs-10 col-xs-offset-1 col-sm-offset-0 col-sm-3 text-sm-right jobTitleGroup">
         <h3 class="jobTitle">Library Tech</h3>
         <p class="jobPlace">Rush University Medical Library</p>
         <p class="jobTime">November 2005 - October 2012</p>
-         </div>
+      </div>
       <div class="col-xs-10 col-xs-offset-1 col-sm-offset-0 col-sm-9 jobDetailsGroup text-left">
-        
+
         <ul>
           <li>Conceived and created the new departmental training and reference Wiki.</li>
           <li>Implemented the latest version of the interlibrary loan software in partnership with the McCormick Educational Technology Center.</li>
           <li>Project planning and execution to reorganize and reorder two major collections (a total of 48,000 books and 4,000 journals) and one minor archival collection (3,500 rare items); each move was completed on time, without disruption to operations or impact on patrons.</li>
-        
+
         </ul>
-        
+
       </div>
 
     </section>
 
-<section class="row">
+    <section class="row">
       <div class="col-xs-12 col-sm-4 ">
         <h2>Competencies</h2>
-        
-      </div>
 
-    </section>
-<section class="row">
-     <div class="col-xs-12  col-sm-5 col-sm-offset-1">
-       <h4>Fig. 1 - Components</h4>
-      <img src="images/partsOfMe.png" alt="" class="img-responsive">
-    </div>
-  <div class="col-xs-12 col-sm-6 ">
-    <h4>Fig. 2 - Skills and Tools</h4>
-      <img src="images/skillsAndTools.png" alt="" class="img-responsive">
-    </div>
+      </div>
 
     </section>
     <section class="row">
-    <div class="col-xs-12 col-sm-4 ">
+      <div class="col-xs-12  col-sm-5 col-sm-offset-1">
+        <h4>Fig. 1 - Components</h4>
+        <img src="images/partsOfMe.png" alt="" class="img-responsive">
+      </div>
+      <div class="col-xs-12 col-sm-6 ">
+        <h4>Fig. 2 - Skills and Tools</h4>
+        <img src="images/skillsAndTools.png" alt="" class="img-responsive">
+      </div>
+
+    </section>
+    <section class="row">
+      <div class="col-xs-12 col-sm-4 ">
         <h2>Education</h2>
       </div>
 
     </section>
     <section class="row">
-    <div class="col-xs-12  ">
+      <div class="col-xs-12  ">
         <p><strong>DePaul University -</strong> Master of Science in Human Computer Interaction, 2016 (expected)- GPA 4.0</p>
       </div>
 
     </section>
-     <section class="row">
-    <div class="col-xs-12  ">
+    <section class="row">
+      <div class="col-xs-12  ">
         <p><strong>University of Michigan -</strong> Bachelor of Fine Arts, April 2005 - GPA 3.50</p>
       </div>
 
     </section>
     <section class="row">
-    <div class="col-xs-12 col-sm-4 ">
+      <div class="col-xs-12 col-sm-4 ">
         <h2>On the Web</h2>
       </div>
 
     </section>
     <section class="row">
-    <div class="col-xs-12 col-sm-8 ">
+      <div class="col-xs-12 col-sm-8 ">
         <p><a href="http://www.linkedin.com/in/nathanpetts">My Linkedin profile - www.linkedin.com/in/nathanpetts</a>
-</p>
+        </p>
       </div>
 
     </section>
-     <section class="row">
-    <div class="col-xs-12 col-sm-8 ">
-        <p><a href="http://www.matec.info/508/">Section 508 Compliance and You! - www.matec.info/508/</a></p>
+    <section class="row">
+      <div class="col-xs-12 col-sm-8 ">
+        <p><a href="http://www.matec.info/508/">Section 508 Compliance and You! - www.matec.info/508/</a>
+        </p>
       </div>
 
     </section>
-
-
-
-
-
     <!--close main-->
-    <article class="continer">
-
-     
-
-
-    </article>
+    
+      <section class="row buttonSection">
+        <?php include "includes/downloadResumeButton.php" ?>
+      </section>
+ 
     <!--close article-->
 
     <footer class=" ">
 
-  <section class="row bottomAlign">
-    <div class="col-xs-1  col-sm-2">
-      <img src="images/goneFishing.png" alt="Pixel Version of Nathan Petts" class="img-responsive  nathanFooter  ">
-    </div>
-    <section class="row col-xs-11 col-sm-4 col-lg-5 mountains">
-      
-      <div class="row col-xs-4 col-xs-offset-5 col-sm-8 col-sm-offset-2  ">
-      <ul class="footerLinks nav nav-pills nav-stacked ">
-        <li class="text-center"><a href="index.php#portfolio">My Portfolio</a>
-        </li>
-        <li class="text-center"><a href="resume.php">My Résumé</a>
-        </li>
-        <li class="text-center"><a href="about.php">About Me</a>
-        </li>
-        <li class="text-center"><a href="contact.php">Contact Me</a>
-        </li>
-      </ul>
-    </div>
-    <!--close row-->
-    <div class="row col-xs-12 col-xs-offset-2 col-sm-offset-0 ">
-      <p class="col-xs-12 copyright">Site <span class="glyphicon glyphicon-copyright-mark"></span>
-        <?php date_default_timezone_set( 'UTC'); echo date( "Y"); ?> Nathan Petts.
-       Maniac Mansion is a copyright of its respective owners. Please do not sue me. </p>
-    </div>
-    <!--close row-->
-    
-    </section>
-    <div class="hidden-xs col-sm-5 col-md-push-1 col-lg-push-1"><img src="images/tree.png" class="img-responisve  footerTree" alt="A pretty tree">
-    </div>
-  </section>
+      <section class="row bottomAlign">
+        <div class="col-xs-1  col-sm-2">
+          <img src="images/goneFishing.png" alt="Pixel Version of Nathan Petts" class="img-responsive  nathanFooter  ">
+        </div>
+        <section class="row col-xs-11 col-sm-4 col-lg-5 mountains">
+
+          <div class="row col-xs-4 col-xs-offset-5 col-sm-8 col-sm-offset-2  ">
+            <ul class="footerLinks nav nav-pills nav-stacked ">
+              <li class="text-center"><a href="index.php#portfolio">My Portfolio</a>
+              </li>
+              <li class="text-center"><a href="resume.php">My Résumé</a>
+              </li>
+              <li class="text-center"><a href="about.php">About Me</a>
+              </li>
+              <li class="text-center"><a href="contact.php">Contact Me</a>
+              </li>
+            </ul>
+          </div>
+          <!--close row-->
+          <div class="row col-xs-12 col-xs-offset-2 col-sm-offset-0 ">
+            <p class="col-xs-12 copyright">Site <span class="glyphicon glyphicon-copyright-mark"></span>
+              <?php date_default_timezone_set( 'UTC'); echo date( "Y"); ?> Nathan Petts. Maniac Mansion is a copyright of its respective owners. Please do not sue me. </p>
+          </div>
+          <!--close row-->
+
+        </section>
+        <div class="hidden-xs col-sm-5 col-md-push-1 col-lg-push-1"><img src="images/tree.png" class="img-responisve  footerTree" alt="A pretty tree">
+        </div>
+      </section>
 
 
 
-  <!--end row-->
+      <!--end row-->
 
 
 
-  <section class="row">
-    <div class="col-xs-12 grass "></div>
+      <section class="row">
+        <div class="col-xs-12 grass "></div>
 
 
-  </section>
+      </section>
 
   </div>
 
