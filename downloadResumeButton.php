4b3d7c919cbbf5ab6f611e400646e8418e3299ea
Nathan Petts <nbpetts@gmail.com>
2015-06-28 09:20:40 -0500

    moved resume button include

--- downloadResumeButton.php
+++
@@ -1,3 +0,0 @@
-<div class="col-xs-4 col-sm-4 col-lg-4  col-sm-offset-1 buttonDiv">
-          <a class="btn btn-default btn-block backToPortfolioButton" href="resume.php" role="button">Download my Résumé</a>
-        </div>
