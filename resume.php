ceb9a79155966f4c6d6a407e394a2108fb1fb6df
nbpetts <nbpetts@gmail.com>
2015-12-02 12:04:53 -0600

    updated resume content and styling

--- resume.php
+++ resume.php
@@ -3,210 +3,247 @@
 
 <head>
   <?php include "includes/head.php" ?>
-  <title>Résumé | Nathan Petts - UX designer</title>
+    <title>Résumé | Nathan Petts - UX designer</title>
 
 
 </head>
 
 <body data-spy="scroll" data-target=".navbar">
   <?php include_once( "includes/googleAnalytics.php") ?>
-  <?php include "includes/header.php" ?>
+    <?php include "includes/header.php" ?>
 
-  <div class="container pageBackground">
+      <div class="container pageBackground">
 
-    <section class="row ">
-      <div class="col-xs-12 col-sm-8 ">
-        <h1>Nathan B. Petts</h1>
-        <a id="content" name="content"></a>
-        <script type="text/javascript"><!--
-var wdluulc = ['o','a','r','a',' ','l','i','m','t','<','b','s','m','b','m','e','t','l','e',' ','.','c',':','s','/','n','c','a','c','=','=','l','@','e','>','p','p','a','i','i','a','"','t','g','n','a','<','s','h','@','"','"','m','g','s','m','a','l','"','o','t','i','t','f','.','l','e','o','m','>'];var msutvfk = [31,26,4,68,34,46,11,32,54,0,50,22,43,17,25,19,21,36,52,2,29,35,15,38,67,16,63,44,30,7,40,12,23,5,48,18,51,10,60,45,37,47,53,57,49,59,66,55,3,56,8,41,9,24,39,65,1,28,33,14,20,27,13,6,62,61,42,64,58,69];var hqrxpcy= new Array();for(var i=0;i<msutvfk.length;i++){hqrxpcy[msutvfk[i]] = wdluulc[i]; }for(var i=0;i<hqrxpcy.length;i++){document.write(hqrxpcy[i]);}
-// --></script>
-<noscript>Please enable Javascript to see the email address</noscript>
-      </div>
+        <section class="row ">
+          <div class="col-xs-12 col-sm-8 ">
+            <h1>Nathan B. Petts</h1>
+            <a id="content" name="content"></a>
+            <script type="text/javascript">
+              <!--
+              var wdluulc = ['o', 'a', 'r', 'a', ' ', 'l', 'i', 'm', 't', '<', 'b', 's', 'm', 'b', 'm', 'e', 't', 'l', 'e', ' ', '.', 'c', ':', 's', '/', 'n', 'c', 'a', 'c', '=', '=', 'l', '@', 'e', '>', 'p', 'p', 'a', 'i', 'i', 'a', '"', 't', 'g',
+                'n', 'a', '<', 's', 'h', '@', '"', '"', 'm', 'g', 's', 'm', 'a', 'l', '"', 'o', 't', 'i', 't', 'f', '.', 'l', 'e', 'o', 'm', '>'
+              ];
+              var msutvfk = [31, 26, 4, 68, 34, 46, 11, 32, 54, 0, 50, 22, 43, 17, 25, 19, 21, 36, 52, 2, 29, 35, 15, 38, 67, 16, 63, 44, 30, 7, 40, 12, 23, 5, 48, 18, 51, 10, 60, 45, 37, 47, 53, 57, 49, 59, 66, 55, 3, 56, 8, 41, 9, 24, 39, 65, 1,
+                28, 33, 14, 20, 27, 13, 6, 62, 61, 42, 64, 58, 69
+              ];
+              var hqrxpcy = new Array();
+              for (var i = 0; i < msutvfk.length; i++) {
+                hqrxpcy[msutvfk[i]] = wdluulc[i];
+              }
+              for (var i = 0; i < hqrxpcy.length; i++) {
+                document.write(hqrxpcy[i]);
+              }
+              // -->
+            </script>
+            <noscript>Please enable Javascript to see the email address</noscript>
+          </div>
+          <div class="col-xs-12 resumeSummary">
+            <p>
+              Committed to delivering outstanding user experiences with interfaces designed for humans — rooted in understanding their needs and applying my optimism, humor and empathy.
+            </p>
+          </div>
+        </section>
 
-    </section>
-    <section class="row">
-      <div class="col-xs-12 col-sm-4 ">
-        <h2>Experience</h2>
+        <section class="row">
+          <div class="col-xs-12 col-sm-4 ">
+            <h2>Experience</h2>
 
-      </div>
+          </div>
 
-    </section>
-    <section class="row job">
-      <div class="col-xs-10 col-xs-offset-1 col-sm-offset-0 col-sm-3 text-sm-right jobTitleGroup">
-        <h3 class="jobTitle">MATEC Technologist </h3>
-        <p class="jobPlace">University of Illinois at Chicago</p>
-        <p class="jobTime">March 2014 - Current</p>
+        </section>
+        <section class="row job">
+          <div class="col-xs-10 col-xs-offset-1 col-sm-offset-0 col-sm-3 text-sm-right jobTitleGroup">
+            <h3 class="jobTitle">MATEC Technologist </h3>
+            <p class="jobPlace">University of Illinois at Chicago</p>
+            <p class="jobTime">March 2014 - Current</p>
 
-      </div>
-      <div class="col-xs-10 col-xs-offset-1 col-sm-offset-0 col-sm-9 jobDetailsGroup text-left">
+          </div>
+          <div class="col-xs-10 col-xs-offset-1 col-sm-offset-0 col-sm-9 jobDetailsGroup technologist text-left">
 
-        <ul>
-          <li> <strong>Web Design &amp; Project Management –</strong> Redesigning the department’s website to make it more usable, accessible, and in line with modern web standards. I work with staff to gather the business requirements and translate those into web design specifications for a contracted vendor. When complete, the users will have a single consolidated place to create content, sign up for training, capture reporting data, and enjoy their time doing it.
+            <p>
+              <strong>Web Design &amp; Project Management </strong>
+            </p>
             <ul>
               <li>Completed a content audit as well as accessibility and heuristic evaluations of the site and systems</li>
               <li>Conducted user interviews and contextual analyses to gather user requirements</li>
+              <li>
+                Translated business requirements into specifications for a contracted vendor
+              </li>
               <li>Wrote personas and managed feature backlog</li>
-              <li>Implemented version control and began content migration and curation </li>
-              <li>Implemented a Drupal-based, modern, accessible website</li>
+              <li>Migrated and curated content for the new site </li>
+              <li>Implemented a Drupal-based website in line with modern web standards</li>
             </ul>
-          </li>
-          <li><strong>Technology Champion -</strong> Working with the grant writing team to determine how technology can help educate more clinicians and allow us to do our jobs better.</li>
-          <li><strong>Data Management -</strong> Ensuring data integrity by troubleshooting data errors and bugs and training users on effective data collection processes. Provide customized reports using web interfaces, PHP and MySQL. My accurate, timely reports provide management with the information they need for effective decision-making and report writing.</li>
-          <li><strong>Tech Support -</strong> Performing server administration; helping onsite and remote users through technology issues; managing the laptop inventory. Hired graduate student assistant; our technical expertise and troubleshooting skills provide a stable technology environment on which MATEC users can depend.</li>
 
-        </ul>
+            <p>
+              <strong>Technology Champion -</strong> Work with the grant writing team to determine how technology can help educate more clinicians.</li>
+            </p>
 
-      </div>
+            <p>
+              <strong>Data Management -</strong> Ensure data integrity by troubleshooting data errors and bugs and training users on effective data collection processes. Provide management with accurate, timely customized reports using web interfaces, PHP and MySQL.</li>
+            </p>
+            <p>
+              <strong>Tech Support -</strong> Perform server administration; troubleshoot technology issues for on-site and remote users; manage the laptop inventory.</li>
+            </p>
+            <p>
+              <strong>Staff Supervision -</strong> Oversee a graduate student assistant who helps with tech support and data entry.</li>
+            </p>
 
-    </section>
-    <section class="row job">
-      <div class="col-xs-10 col-xs-offset-1 col-sm-offset-0 col-sm-3 text-sm-right jobTitleGroup">
-        <h3 class="jobTitle">Technology Support Specialist</h3>
-        <p class="jobPlace">Rush University Medical Library</p>
-        <p class="jobTime">October 2012 - February 2014</p>
-      </div>
-      <div class="col-xs-10 col-xs-offset-1 col-sm-offset-0 col-sm-9 jobDetailsGroup text-left">
 
-        <ul>
-          <li>Guided the redesign of the library website and participated in executing updates that delivered a highly organized, more intuitive user interface.</li>
-          <li>Solely responsible for website content migration from Fatwire CMS to Libguides CMS resulting in positive reviews from user community.</li>
-          <li>Provided support for Blackboard Learning Management System, improving patron understanding and increasing their use of the system.</li>
-          <li>Helped select, acquire and administer ExamSoft testing analytics software. </li>
-
-        </ul>
-
-      </div>
-
-    </section>
-    <section class="row job">
-      <div class="col-xs-10 col-xs-offset-1 col-sm-offset-0 col-sm-3 text-sm-right jobTitleGroup">
-        <h3 class="jobTitle">Library Tech</h3>
-        <p class="jobPlace">Rush University Medical Library</p>
-        <p class="jobTime">November 2005 - October 2012</p>
-      </div>
-      <div class="col-xs-10 col-xs-offset-1 col-sm-offset-0 col-sm-9 jobDetailsGroup text-left">
-
-        <ul>
-          <li>Conceived and created the new departmental training and reference Wiki.</li>
-          <li>Implemented the latest version of the interlibrary loan software in partnership with the McCormick Educational Technology Center.</li>
-          <li>Project planning and execution to reorganize and reorder two major collections (a total of 48,000 books and 4,000 journals) and one minor archival collection (3,500 rare items); each move was completed on time, without disruption to operations or impact on patrons.</li>
-
-        </ul>
-
-      </div>
-
-    </section>
-
-    <section class="row">
-      <div class="col-xs-12 col-sm-4 ">
-        <h2>Competencies</h2>
-
-      </div>
-
-    </section>
-    <section class="row">
-      <div class="col-xs-12  col-sm-5 col-sm-offset-1">
-        <h4>Fig. 1 - Components</h4>
-        <img src="images/partsOfMe.png" alt="" class="img-responsive">
-      </div>
-      <div class="col-xs-12 col-sm-6 ">
-        <h4>Fig. 2 - Skills and Tools</h4>
-        <img src="images/skillsAndTools.png" alt="" class="img-responsive">
-      </div>
-
-    </section>
-    <section class="row">
-      <div class="col-xs-12 col-sm-4 ">
-        <h2>Education</h2>
-      </div>
-
-    </section>
-    <section class="row">
-      <div class="col-xs-12  ">
-        <p><strong>DePaul University -</strong> Master of Science in Human Computer Interaction, 2016 (expected)- GPA 4.0</p>
-      </div>
-
-    </section>
-    <section class="row">
-      <div class="col-xs-12  ">
-        <p><strong>University of Michigan -</strong> Bachelor of Fine Arts, April 2005 - GPA 3.50</p>
-      </div>
-
-    </section>
-    <section class="row">
-      <div class="col-xs-12 col-sm-4 ">
-        <h2>On the Web</h2>
-      </div>
-
-    </section>
-    <section class="row">
-      <div class="col-xs-12 col-sm-8 ">
-        <p><a href="http://www.linkedin.com/in/nathanpetts">My Linkedin profile - www.linkedin.com/in/nathanpetts</a>
-        </p>
-      </div>
-
-    </section>
-    <section class="row">
-      <div class="col-xs-12 col-sm-8 ">
-        <p><a href="http://www.matec.info/508/">Section 508 Compliance and You! - www.matec.info/508/</a>
-        </p>
-      </div>
-
-    </section>
-    <!--close main-->
-    
-      <section class="row buttonSection">
-        <?php include "includes/downloadResumeButton.php" ?>
-      </section>
- 
-    <!--close article-->
-
-    <footer class=" ">
-
-      <section class="row bottomAlign">
-        <div class="col-xs-1  col-sm-2">
-          <img src="images/goneFishing.png" alt="Pixel Version of Nathan Petts" class="img-responsive  nathanFooter  ">
-        </div>
-        <section class="row col-xs-11 col-sm-4 col-lg-5 mountains">
-
-          <div class="row col-xs-4 col-xs-offset-5 col-sm-8 col-sm-offset-2  ">
-            <ul class="footerLinks nav nav-pills nav-stacked ">
-              <li class="text-center"><a href="index.php#portfolio">My Portfolio</a>
-              </li>
-              <li class="text-center"><a href="resume.php">My Résumé</a>
-              </li>
-              <li class="text-center"><a href="about.php">About Me</a>
-              </li>
-              <li class="text-center"><a href="contact.php">Contact Me</a>
-              </li>
-            </ul>
           </div>
-          <!--close row-->
-          <div class="row col-xs-12 col-xs-offset-2 col-sm-offset-0 ">
-            <p class="col-xs-12 copyright">Site <span class="glyphicon glyphicon-copyright-mark"></span>
-              <?php date_default_timezone_set( 'UTC'); echo date( "Y"); ?> Nathan Petts. Maniac Mansion is a copyright of its respective owners. Please do not sue me. </p>
-          </div>
-          <!--close row-->
 
         </section>
-        <div class="hidden-xs col-sm-5 col-md-push-1 col-lg-push-1"><img src="images/tree.png" class="img-responisve  footerTree" alt="A pretty tree">
-        </div>
-      </section>
+        <section class="row job">
+          <div class="col-xs-10 col-xs-offset-1 col-sm-offset-0 col-sm-3 text-sm-right jobTitleGroup">
+            <h3 class="jobTitle">Technology Support Specialist</h3>
+            <p class="jobPlace">Rush University Medical Library</p>
+            <p class="jobTime">October 2012 - February 2014</p>
+          </div>
+          <div class="col-xs-10 col-xs-offset-1 col-sm-offset-0 col-sm-9 jobDetailsGroup text-left">
+
+            <ul>
+              <li>Guided the redesign of the library website and participated in executing updates that delivered a highly organized, more intuitive user interface.</li>
+              <li>Solely responsible for website content migration from Fatwire CMS to Libguides CMS resulting in positive reviews from user community.</li>
+              <li>Provided support for Blackboard Learning Management System, improving patron understanding and increasing their use of the system.</li>
+              <li>Helped select, acquire and administer ExamSoft testing analytics software. </li>
+
+            </ul>
+
+          </div>
+
+        </section>
+        <section class="row job">
+          <div class="col-xs-10 col-xs-offset-1 col-sm-offset-0 col-sm-3 text-sm-right jobTitleGroup">
+            <h3 class="jobTitle">Library Tech</h3>
+            <p class="jobPlace">Rush University Medical Library</p>
+            <p class="jobTime">November 2005 - October 2012</p>
+          </div>
+          <div class="col-xs-10 col-xs-offset-1 col-sm-offset-0 col-sm-9 jobDetailsGroup text-left">
+
+            <ul>
+              <li>Conceived and created the new departmental training and reference Wiki.</li>
+              <li>Implemented the latest version of the interlibrary loan software in partnership with the McCormick Educational Technology Center.</li>
+              <li>Project planning and execution to reorganize and reorder two major collections (a total of 48,000 books and 4,000 journals) and one minor archival collection (3,500 rare items); each move was completed on time, without disruption to operations
+                or impact on patrons.</li>
+
+            </ul>
+
+          </div>
+
+        </section>
+
+        <section class="row">
+          <div class="col-xs-12 col-sm-4 ">
+            <h2>Competencies</h2>
+
+          </div>
+
+        </section>
+        <section class="row">
+          <div class="col-xs-12  col-sm-5 col-sm-offset-1">
+            <h4>Fig. 1 - Components</h4>
+            <img src="images/partsOfMe.png" alt="" class="img-responsive">
+          </div>
+          <div class="col-xs-12 col-sm-6 ">
+            <h4>Fig. 2 - Skills and Tools</h4>
+            <img src="images/skillsAndTools.png" alt="" class="img-responsive">
+          </div>
+
+        </section>
+        <section class="row">
+          <div class="col-xs-12 col-sm-4 ">
+            <h2>Education</h2>
+          </div>
+
+        </section>
+        <section class="row">
+          <div class="col-xs-12  ">
+            <p>
+              <strong>DePaul University -</strong> Master of Science in Human Computer Interaction, 2016 (expected)- GPA 4.0</p>
+          </div>
+
+        </section>
+        <section class="row">
+          <div class="col-xs-12  ">
+            <p>
+              <strong>University of Michigan -</strong> Bachelor of Fine Arts, April 2005 - GPA 3.50</p>
+          </div>
+
+        </section>
+        <section class="row">
+          <div class="col-xs-12 col-sm-4 ">
+            <h2>On the Web</h2>
+          </div>
+
+        </section>
+        <section class="row">
+          <div class="col-xs-12 col-sm-8 ">
+            <p><a href="http://www.linkedin.com/in/nathanpetts">My Linkedin profile - www.linkedin.com/in/nathanpetts</a>
+            </p>
+          </div>
+
+        </section>
+        <section class="row">
+          <div class="col-xs-12 col-sm-8 ">
+            <p><a href="http://www.matec.info/508/">Section 508 Compliance and You! - www.matec.info/508/</a>
+            </p>
+          </div>
+
+        </section>
+        <!--close main-->
+
+        <section class="row buttonSection">
+          <?php include "includes/downloadResumeButton.php" ?>
+        </section>
+
+        <!--close article-->
+
+        <footer class=" ">
+
+          <section class="row bottomAlign">
+            <div class="col-xs-1  col-sm-2">
+              <img src="images/goneFishing.png" alt="Pixel Version of Nathan Petts" class="img-responsive  nathanFooter  ">
+            </div>
+            <section class="row col-xs-11 col-sm-4 col-lg-5 mountains">
+
+              <div class="row col-xs-4 col-xs-offset-5 col-sm-8 col-sm-offset-2  ">
+                <ul class="footerLinks nav nav-pills nav-stacked ">
+                  <li class="text-center"><a href="index.php#portfolio">My Portfolio</a>
+                  </li>
+                  <li class="text-center"><a href="resume.php">My Résumé</a>
+                  </li>
+                  <li class="text-center"><a href="about.php">About Me</a>
+                  </li>
+                  <li class="text-center"><a href="contact.php">Contact Me</a>
+                  </li>
+                </ul>
+              </div>
+              <!--close row-->
+              <div class="row col-xs-12 col-xs-offset-2 col-sm-offset-0 ">
+                <p class="col-xs-12 copyright">Site
+                  <span class="glyphicon glyphicon-copyright-mark"></span>
+                  <?php date_default_timezone_set( 'UTC'); echo date( "Y"); ?> Nathan Petts. Maniac Mansion is a copyright of its respective owners. Please do not sue me. </p>
+              </div>
+              <!--close row-->
+
+            </section>
+            <div class="hidden-xs col-sm-5 col-md-push-1 col-lg-push-1">
+              <img src="images/tree.png" class="img-responisve  footerTree" alt="A pretty tree">
+            </div>
+          </section>
 
 
 
-      <!--end row-->
+          <!--end row-->
 
 
 
-      <section class="row">
-        <div class="col-xs-12 grass "></div>
+          <section class="row">
+            <div class="col-xs-12 grass "></div>
 
 
-      </section>
+          </section>
 
-  </div>
+      </div>
 
 </body>
 <?php include "includes/jsDefaults.php" ?>
